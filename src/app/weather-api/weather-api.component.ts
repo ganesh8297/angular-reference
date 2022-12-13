ab024c9f18f93d50a240696a5ef0f62aed9b20cb
Venkat <venkatachalam.dev05@gmail.com>
2022-12-13 11:48:47 +0530

    Weather API and Export Excel

--- src/app/weather-api/weather-api.component.ts
+++ src/app/weather-api/weather-api.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-weather-api',
   templateUrl: './weather-api.component.html',
@@ -7,19 +8,66 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 })
 export class WeatherApiComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
 
+  weatherDetails: any = [{
+    mintemp: "",
+    maxtemp: ""
+  }]
+  error: any = ""
+  alert: any = false;
+
+
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
+    this.couterCalculation();
+  }
+
+
+
+  getweather(data: string) {
+    this.http.get("https://api.openweathermap.org/data/2.5/weather?q=" + data + "&units=metric&APPID=eb03b1f5e5afb5f4a4edb40c1ef2f534").subscribe(
+      (data: any) => {
+        console.log(data);
+        this.weatherDetails.mintemp = data.main.temp_max;
+        this.weatherDetails.maxtemp = data.main.temp_min;
+        this.alert = false;
+      },
+      error => {
+        this.alert = true;
+      }
+    );
+  }
+
+  exportData(): void {
+
+    /* pass here the table id */
+    let element = document.getElementById('excel-table');
+    let element2 = document.getElementById('excel-table');
+    const sheet1: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+    const sheet2: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element2);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, sheet1, 'Sheet1');
+    XLSX.utils.book_append_sheet(wb, sheet2, 'Sheet2');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'WeatherData.xlsx');
 
   }
 
-  error: any = ""
-  alert: any = false;
-  getweather(data: string) {
-    this.http.get("https://api.openweathermap.org/data/2.5/weather?q=" + data + "&units=metric&APPID=eb03b1f5e5afb5f4a4edb40c1ef2f534").subscribe(
-      data => console.log(data),
-      error => this.alert = true
-    )
+  days: number = 100;
+  hours: number = 10;
+  mins: number = 10;
+  seconds: number = 10;
+
+  couterCalculation() {
+    var futureDate: any = new Date("Tue Dec 13 2023 11:42:28");
+    var today: any = new Date();
+
+
+
+
   }
 
 }
